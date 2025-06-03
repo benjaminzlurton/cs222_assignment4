1eabff3f8e6f71616f924743a47f761e3ea89014
benajminzlurton <@>
2025-06-02 23:05:55 -0400

    Commit

---
+++ assignment4.py
@@ -0,0 +1,8 @@
+import json
+import urllib.request
+
+weatherData = json.loads(urllib.request.urlopen("https://api.weather.gov/points/40.1934,-85.3864").read())
+weatherForecast = json.loads(urllib.request.urlopen(weatherData["properties"]["forecast"]).read())
+
+for forecastPeriod in weatherForecast["properties"]["periods"]:
+    print(f"{forecastPeriod["name"]} - {forecastPeriod["temperature"]}°F \n{forecastPeriod["detailedForecast"]}\n")
