4eb339f305715fd4c53f1849d4c1bdc256d56a17
benajminzlurton <@>
2025-06-06 20:40:51 -0400

    fix for old python

--- assignment4.py
+++ assignment4.py
@@ -4,5 +4,5 @@ import urllib.request
 weatherData = json.loads(urllib.request.urlopen("https://api.weather.gov/points/40.1934,-85.3864").read())
 weatherForecast = json.loads(urllib.request.urlopen(weatherData["properties"]["forecast"]).read())
 
-for forecastPeriod in weatherForecast["properties"]["periods"]:
-    print(f"{forecastPeriod["name"]} - {forecastPeriod["temperature"]}°F \n{forecastPeriod["detailedForecast"]}\n")
+for forecastPeriod in weatherForecast['properties']['periods']:
+    print(f"{forecastPeriod['name']} - {forecastPeriod['temperature']}°F \n{forecastPeriod['detailedForecast']}\n")
